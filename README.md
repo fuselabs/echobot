3b93518c140b1de1fc2bacc709757cfca6f2c0fd
Dan Marshall <danmar@microsoft.com>
2016-04-05 16:03:12 -0700

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # echobot
 A sample bot for getting started with Bot Framework
+
+This repo is an example of using Node.js to build a bot, which is hosted on Azure and uses continuous deployment from Github.
+
+Here's how to use this bot as a starter template for your own Node.js based bot:
+
+1. Fork this repo.
+2. Create an Azure web app.
+3. Set up continuous deployment to Azure from your Github repo.
+4. Register your bot with the Bot Framework.
+5. Enter your Bot Framework App ID and App Secret into Azure settings.
+6. Chat with your bot.
+   
---
+++ server.js
@@ -0,0 +1,32 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Get secrets from server environment
+var botConnectorOptions = { 
+    appId: process.env.BOTFRAMEWORK_APPID, 
+    appSecret: process.env.BOTFRAMEWORK_APPSECRET 
+};
+
+// Create bot
+var bot = new builder.BotConnectorBot(botConnectorOptions);
+bot.add('/', function (session) {
+    
+    //respond with user's message
+    session.send(session.message.text);
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+
+// Handle Bot Framework messages
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+	'directory': '.',
+	'default': 'index.html'
+}));
+
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
