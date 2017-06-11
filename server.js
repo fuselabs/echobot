24c2c1639db205f11c1b260a37988ac0f7bf6374
rameshananth <rameshananth@users.noreply.github.com>
2017-06-11 10:36:28 +0530

    Create server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var bot = new builder.UniversalBot(connector);
 bot.dialog('/', function (session) {
     
     //respond with user's message
-    session.send("You said : " + session.message.text);
+    session.send("Hi this is crashcart! How can I help you");
 });
 
 // Setup Restify Server
