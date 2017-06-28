d38228c75b104f0495483d68d30bef763d74bae4
rameshananth <rameshananth@users.noreply.github.com>
2017-06-28 12:49:48 +0530

    Update helper.js

--- botframework/prompts/helper.js
+++ botframework/prompts/helper.js
@@ -2,16 +2,30 @@ var builder=require('botbuilder');
 
 var lib=new builder.Library('MSBotFramework');
 
+lib.dialog('/GetConfirm',[
+  function(session,args,next){
+    console.log("In the MSBotFramework:/GetConfirm function");
+    builder.Prompts.confirm(session,args.message);
+  },
+  function(session,result){
+    console.log('Result returned from MSBotFramework:/GetConfirm function is in variable: response'
+                +'Result is:'+result.response
+                );
+    session.endDialogWithResult({response:result.response,resumed:null});
+  }
+]);
+                
+
 lib.dialog('/GetText',[
   function(session,args,next){
     console.log("In the MSBotFramework:/GetText function");
-    console.log("The return variable is:"+args.returnVariable);
+    //console.log("The return variable is:"+args.returnVariable);
     session.dialogData.returnVariable=args.returnVariable;
     builder.Prompts.text(session,args.message);
   },
   function(session,result){
     var sResult='response.'+session.dialogData.returnVariable;
-    console.log('Result returned from MSBotFramework:/GetText function is in variable:'+sResult
+    console.log('Result returned from MSBotFramework:/GetText function is in variable: response'
                 +'Result is:'+result.response
                );
     var map={}
