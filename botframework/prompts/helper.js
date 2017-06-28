da5a52e8b878da023003676f000165d48b266db8
rameshananth <rameshananth@users.noreply.github.com>
2017-06-28 10:53:22 +0530

    Abstract getting user prompts from MSBotFramework
    1. Added dialog to prompt user for text input

---
+++ botframework/prompts/helper.js
@@ -0,0 +1,18 @@
+var builder=require('botbuilder');
+
+var lib=new builder.Library('MSBotFramework');
+
+lib.dialog('/GetText',[
+  function(session,args,next){
+    console.log("In the MSBotFramework:/GetText function");
+    console.log("The return variable is:"+args.returnVariable);
+    session.dialogData.returnVariable=args.returnVariable;
+    builder.prompt.Text(session,args.message);
+  },
+  function(session,results){
+    session.endDialogWithResults({
+      'response.'+session.dialogData.returnVariable:results.response,
+      'resumed':null
+    });
+  }
+]);
