0b3cdf5b5f461bc95331ab014d94a6d3e660b2de
rameshananth <rameshananth@users.noreply.github.com>
2017-06-28 16:59:50 +0530

    Updated /GetTicket

--- itsm/servicenow/helper.js
+++ itsm/servicenow/helper.js
@@ -24,13 +24,21 @@ lib.dialog('/GetTickets',[
 
 lib.dialog('/GetTicket',[
 		function(session,args,next){
+			/*
+			TODO: 
+			1. Need to rewrite this function so that the function evals the args and gets the number from the args.
+			right now implicitly picks it up from session.conversationData
+			Looks up a ticketnumber available in session.conversationData.Ticket
+			Remember to endConversation at the handler function
+			*/
 			console.log("In the ServiceNow:/GetTicket function");
 			console.log(args);
 			console.log("Finding ticket:"+args.ticket_number);
 			var uName=session.message.address.user.name;
 			var Snow=new serviceNow('https://wiprodemo4.service-now.com/','admin','LWP@2015');
 			var tickets;
-			var number;
+			
+			var number=session.conversationData.Ticket;
 			/*
 			if(args.type=='entity'){
 				number=args.ticket_number.entity;
