fc924625a661c15bd06d476071b42e07473d24c7
rameshananth <rameshananth@users.noreply.github.com>
2017-07-05 14:07:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -59,6 +59,10 @@ server.get(/.*/, restify.serveStatic({
 	'default': 'index.html'
 }));
 
+server.get(/convs\/ServiceDesk/,restify.serveStatic({
+	'directory':'convs/ServiceDesk/',
+	'file':'index.html'
+}));
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
