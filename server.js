8e5de7b8074f648d34d05ff34d7de899618232f7
rameshananth <rameshananth@users.noreply.github.com>
2017-07-05 15:04:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -54,15 +54,17 @@ var server = restify.createServer();
 server.post('/api/messages', connector.listen());
 
 // Serve a static web page
+
+server.get(/\/convs\/ServiceDesk\/?.*/,restify.serveStatic({
+	'directory': __dirname,
+	'default':'index.html'
+}));
+
 server.get(/.*/, restify.serveStatic({
 	'directory': '.',
 	'default': 'index.html'
 }));
 
-server.get(/\/convs\/ServiceDesk\/?.*/,restify.serveStatic({
-	'directory': __dirname,
-	'file':'index.html'
-}));
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
